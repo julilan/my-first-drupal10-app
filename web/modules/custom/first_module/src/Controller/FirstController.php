2649ca0ffbffd856316ba240f8d2fad96579a32a
julilan <juli.molnar@outlook.com>
2023-08-30 10:34:45 +0300

    add form_alter hook

--- web/modules/custom/first_module/src/Controller/FirstController.php
+++ web/modules/custom/first_module/src/Controller/FirstController.php
@@ -3,19 +3,14 @@
 namespace Drupal\first_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
 
 class FirstController extends ControllerBase
 {
-
   public function first()
   {
-
-    $p = [
-      '#type' => 'html_tag',
-      '#tag' => 'p',
-      '#value' => $this->t('This is the first module'),
+    // return array('#markup' => 'Welcome to our Website.');
+    return [
+      '#markup' => $this->t('Welcome to our Website also included translation'),
     ];
-    return $p;
   }
 }
