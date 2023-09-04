90d9ea4f5172ee2326bdfa42336f397ebcf881e3
julilan <juli.molnar@outlook.com>
2023-09-04 13:45:52 +0300

    add react

---
+++ web/themes/custom/react_hello/js/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Simple "Drupal - React Together"
+ReactDOM.render(
+  <h1>Drupal - React Together, again!</h1>,
+  document.getElementById("react-app")
+);
